88e35ac4062ebb4ca1883a5b96f9e59377118bc3
さいとうはじめ <saitohajime@saitouhajimenoMacBook-Pro.local>
2021-01-07 19:44:28 +0900

    yes

--- controllers/posts.js
+++ controllers/posts.js
@@ -10,13 +10,12 @@ export const getPosts = async (req, res) => {
         res.status(200).json(postMessages);
 
     } catch (error) {
-        res.status(404).json({message: errer.message})
+        res.status(404).json({message: error.message})
     }
 };
 
 export const createPost = async (req, res) => {
     const post = req.body;
-
     const newPost = new PostMessage(post);
 
     try {
@@ -34,7 +33,6 @@ export const updatePost = async (req, res) => {
 
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
 
-    
     const updatedPost = await PostMessage.findByIdAndUpdate(_id, {...post, _id}, { new: true });
 
     res.json(updatedPost);
